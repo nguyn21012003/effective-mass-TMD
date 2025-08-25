72891ae2571c13e736c3fc2366072f4d789d829b
nguyn21012003 <66507352+nguyn21012003@users.noreply.github.com>
2025-08-25 18:22:09 +0700

    update find cross function

--- detectCross.py
+++ detectCross.py
@@ -1,41 +1,70 @@
 from os import defpath
 import pandas as pd
-from copy import deepcopy
+from copy import copy, deepcopy
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-def sortCrossing(listEigen, level1, level2):
-    arr1 = deepcopy(listEigen[level1])
-    arr2 = deepcopy(listEigen[level2])
-    for i in range(1, len(arr1)):
-        deltaE_prev = arr1[i] - arr1[i - 1]
-        deltaE_curr = arr2[i] - arr1[i]
-        print(deltaE_prev)
-        if deltaE_prev * deltaE_curr < 0:
-            tmp1 = deepcopy(arr1[i:])
-            tmp2 = deepcopy(arr2[i:])
-            arr1[i:], arr2[i:] = tmp2, tmp1
+def track_eigenvalues(eigenArrInit, Barr):
 
-    listEigen[level1] = arr1
-    listEigen[level2] = arr2
+    arr1 = None
+    arr2 = None
+    arr3 = None
+    arr4 = None
+    keys = []
+    eigenArrNew = deepcopy(eigenArrInit)
+    for key in eigenArrInit:
+        keys.append(key)
+    for qi in range(0, len(keys) - 3):
+
+        key1 = keys[qi + 0]
+        key2 = keys[qi + 1]
+        key3 = keys[qi + 2]
+        key4 = keys[qi + 3]
+
+        arr1 = copy(eigenArrInit[key1])
+        arr2 = copy(eigenArrInit[key2])
+        arr3 = copy(eigenArrInit[key3])
+        arr4 = copy(eigenArrInit[key4])
+
+        for i in range(1, len(Barr) - 1):
+            prev = arr1[i - 1]
+            cur = arr1[i]
+            next = arr1[i + 1]
+            if arr1[i] > arr3[i]:
+                # if (cur - prev) * (next - cur) < 0:
+                print(Barr[i])
+                arr1[i:], arr3[i:] = copy(arr3[i:]), copy(arr1[i:])
+                arr2[i:], arr4[i:] = copy(arr4[i:]), copy(arr2[i:])
+        eigenArrNew[key1] = arr1
+        eigenArrNew[key2] = arr2
+        eigenArrNew[key3] = arr3
+        eigenArrNew[key4] = arr4
+
+    for k in keys[:6]:
+        plt.plot(Barr, eigenArrNew[k], label=k)
+    plt.legend()
+    plt.show()
+
     return None
 
 
 def main():
-    NNdir = "./Sat-08-23/NN/"
-    TNNdir = "./Sat-08-23/TNN/"
+    NNdir = "./Sun-08-24/NN/"
+    TNNdir = "./Sun-08-24/TNN/"
 
-    dataframe = pd.read_csv(TNNdir + "3band_Lambda2q_dataHofstadterButterfly_q_2001_MoS2_GGA_G.dat")
+    dataframe = pd.read_csv(NNdir + "3band_Lambda2q_dataHofstadterButterfly_q_797_MoS2_GGA_G.dat")
     dataframe = dataframe.drop(columns=["evalues"])
 
     numLevels = 80
     lv1 = "E2q17"
     lv2 = "E2q20"
 
-    listEigen = {}
+    listAllEigen = {}
+    B_arr = dataframe["B_values"].values
     for i in range(numLevels + 1):
-        listEigen[f"E2q{i}"] = dataframe[dataframe.columns[i + 2]].values
-
-    print(sortCrossing(listEigen, lv1, lv2))
+        listAllEigen[f"E2q{i}"] = dataframe[f"E2q{i}"].values
+    track_eigenvalues(listAllEigen, B_arr)
 
     return None
 
